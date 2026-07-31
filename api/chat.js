7a45df4367e0305fe058dda31dd9cdf7775dbd30
Marwan Shehata <shehatamarwan56@gmail.com>
2026-07-31 05:38:42 +0300

    Restore setCorsHeaders function in api/chat.js

--- api/chat.js
+++ api/chat.js
@@ -130,8 +130,15 @@ function generateSmartRapunzelFallback(message, currentlyAnnoyed) {
     mood: "NEUTRAL",
     energyDelta: +5
   };
+function setCorsHeaders(res) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
 }
-
 
 function cleanChatReply(text) {
   if (!text) return "أنا لولا! عاملة إيه يا أيويتي؟";
